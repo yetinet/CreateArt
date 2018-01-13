74e669210912bba2e1c514793d8d656c0a13f242
Ubuntu <ubuntu@ip-172-31-80-124.ec2.internal>
2018-01-13 07:42:51 +0000

    make images directories in deploy spript

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,12 @@
 sudo apt-get install python3-setuptools
 sudo easy_install3 pip
 pip3 install mypackage --user
-git clone https://github.com/yetinet/CreateArt.git
-cd CreateArt
 pip3 install -r CreateArt-Req.txt --user
 sudo apt-get install python3-tk
+
+git clone https://github.com/yetinet/CreateArt.git
+
+mkdir CreateArt/images/style
+mkdir CreateArt/images/content
+mkdir CreateArt/output
 
